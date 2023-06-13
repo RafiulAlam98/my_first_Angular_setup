1bc4d7d922420d42fa7937edd018a642a6a5ecb7
hasib-git <hasibgit@gmail.com>
2023-06-13 12:42:29 +0600

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
